2b5c234005057eea76d0d495c7aed7f152199c67
Wazid Hossain <mwh.wajid96@gmail.com>
2025-02-06 12:27:39 +0600

    colors changers and remember me button added

--- lib/login.dart
+++ lib/login.dart
@@ -11,133 +11,178 @@ class Login_page extends StatefulWidget {
 class _Login_pageState extends State<Login_page> {
   bool _obscureText = true;
   int currentIndex = 1;
+  bool _rememberMe = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       appBar: AppBar(
         elevation: 0,
         title: const Text(
-          'ZEO TEXT BD',
+          'ZEO TEX BD',
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Colors.indigo,
+        backgroundColor: Colors.cyan,
       ),
       body: Center(
         child: Center(
           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
             Center(
-              child: Container(
-                height: 400,
-                width: 400,
-                decoration: BoxDecoration(
-                  color: Colors.grey,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'ZEO TEXT BD',
-                      style: TextStyle(
-                        color: Colors.indigo,
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Container(
+                  height: 400,
+                  width: 400,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 5,
+                        offset: const Offset(0, 5),
                       ),
-                    ),
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'ZEO TEXT BD',
+                        style: TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
 
-                    // To input Iamge from the internet letter
-                    Column(
-                      children: [
-                        Center(
-                          child: TextField(
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
+                      // To input Iamge from the internet letter
+                      Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          children: [
+                            Center(
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Colors.indigo, width: 2),
+                                    borderRadius: BorderRadius.all(Radius.zero),
+                                  ),
+                                  hintText: 'example@abc.com',
+                                  hintStyle: TextStyle(color: Colors.black),
+                                  label: Text('Email',
+                                      style: TextStyle(
+                                        color: Colors.cyan,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      )),
+                                  prefixIcon: Icon(Icons.email_outlined),
+                                ),
+                                keyboardType: TextInputType.emailAddress,
                               ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.indigo, width: 2),
-                                borderRadius: BorderRadius.all(Radius.zero),
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.indigo, width: 2),
+                                  borderRadius: BorderRadius.all(Radius.zero),
+                                ),
+                                hintText: 'Enter your password',
+                                hintStyle: TextStyle(color: Colors.black),
+                                label: Text(
+                                  'Password',
+                                  style: TextStyle(
+                                      color: Colors.cyan,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                prefixIcon: Icon(Icons.lock_outline),
+                                // suffixIcon: Icon(Icons.visibility_off_outlined),
+                                // for click the eye icon to show the password we use the below code
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText = !_obscureText;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    _obscureText
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                  ),
+                                ),
+                                //or if we want to click the eye icon or any other icon we use gesture detector or inkwell
                               ),
-                              hintText: 'example@abc.com',
-                              hintStyle: TextStyle(color: Colors.black),
-                              label: Text('Email'),
-                              prefixIcon: Icon(Icons.email_outlined),
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: true,
                             ),
-                            keyboardType: TextInputType.emailAddress,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            SizedBox(
+                              height: 40,
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: Colors.indigo, width: 2),
-                              borderRadius: BorderRadius.all(Radius.zero),
-                            ),
-                            hintText: 'Enter your password',
-                            hintStyle: TextStyle(color: Colors.black),
-                            label: Text('Password'),
-                            prefixIcon: Icon(Icons.lock_outline),
-                            // suffixIcon: Icon(Icons.visibility_off_outlined),
-                            // for click the eye icon to show the password we use the below code
-                            suffixIcon: IconButton(
-                              onPressed: () {
+                            Checkbox(
+                              value: _rememberMe,
+                              onChanged: (value) {
                                 setState(() {
-                                  _obscureText = !_obscureText;
+                                  _rememberMe = value!;
                                 });
+                                Text('Remember Me');
                               },
-                              icon: Icon(
-                                _obscureText
-                                    ? Icons.visibility
-                                    : Icons.visibility_off,
+                            ),
+                            // Text('Remember Me'),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Login_page(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Login',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.cyanAccent,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 150, vertical: 10),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
                             ),
-                            //or if we want to click the eye icon or any other icon we use gesture detector or inkwell
-                          ),
-                          keyboardType: TextInputType.visiblePassword,
-                          obscureText: true,
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Login_page(),
-                              ),
-                            );
-                          },
-                          child: const Text(
-                            'Login',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
+                            SizedBox(
+                              height: 50,
                             ),
-                          ),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.cyanAccent,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 150, vertical: 10),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
+                            Text(
+                              'version: 3.2',
+                              style: TextStyle(
+                                  color: Colors.cyan,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
                         ),
-                      ],
-                    )
-                    // To input Iamge from the internet letter
-                  ],
+                      )
+                      // To input Iamge from the internet letter
+                    ],
+                  ),
                 ),
               ),
             ),
