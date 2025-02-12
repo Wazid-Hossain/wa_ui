7e0aa672adae52ed8a3467d109276aa4c7dc0c06
Wazid Hossain <mwh.wajid96@gmail.com>
2025-02-12 15:59:43 +0600

    some error fixed

--- lib/api_services/apiServices.dart
+++ lib/api_services/apiServices.dart
@@ -5,11 +5,11 @@ import '../model.dart';
 class Apiservices {
   Future<LoginModel?> loginwithmodel(String email, String password) async {
     try {
-      var url = Uri.parse("https://zeotexbd.com//api/login");
+      var url = Uri.parse("https://reqres.in/api/login");
       var respone =
           await http.post(url, body: {"email": email, "password": password});
 
-      if (respone.statusCode == 401) {
+      if (respone.statusCode == 200) {
         LoginModel model = LoginModel.fromJson(jsonDecode(respone.body));
         return model;
       }
